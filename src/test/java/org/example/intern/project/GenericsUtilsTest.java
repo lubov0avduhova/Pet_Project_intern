a97a9537d6863e4b0ff7d21dc9fa95fb4d89c532
Lubov Avduhova <loveavduhova@gmail.com>
2025-08-13 18:26:34 +0700

    Реализовать универсальный метод для получения первого элемента списка - 1 реализация

---
+++ src/test/java/org/example/intern/project/GenericsUtilsTest.java
@@ -0,0 +1,43 @@
+package org.example.intern.project;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GenericsUtilsTest {
+    @Test
+    public void getFirstElement_shouldReturnNull_whenListIsNull(){
+        Assertions.assertNull(GenericsUtils.getFirstElement(null));
+    }
+
+    @Test
+    public void getFirstElement_shouldReturnNull_whenListIsEmpty(){
+        List<String> list = new ArrayList<>();
+
+        Assertions.assertNull(GenericsUtils.getFirstElement(list));
+    }
+
+    @Test
+    public void getFirstElement_shouldReturnObject_whenListHasOneElement(){
+        String firstElement = "firstElement";
+        List<String> list = new ArrayList<>();
+        list.add(firstElement);
+
+        Assertions.assertEquals(firstElement, GenericsUtils.getFirstElement(list));
+    }
+
+    @Test
+    public void getFirstElement_shouldReturnObject_whenListHasMoreThanOneElement(){
+        String firstElement = "firstElement";
+        String secondElement = "secondElement";
+        String thirdElement = "thirdElement";
+        List<String> list = new ArrayList<>();
+        list.add(firstElement);
+        list.add(secondElement);
+        list.add(thirdElement);
+
+        Assertions.assertEquals(firstElement, GenericsUtils.getFirstElement(list));
+    }
+}
