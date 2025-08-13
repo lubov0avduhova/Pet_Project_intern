400a76bef47963c8e6b4904acc3042321bbb5b3c
Lubov Avduhova <loveavduhova@gmail.com>
2025-08-13 09:44:19 +0700

    Initial commit

---
+++ src/main/java/org/example/intern_project/InternProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.intern_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InternProjectApplication.class, args);
+    }
+
+}
