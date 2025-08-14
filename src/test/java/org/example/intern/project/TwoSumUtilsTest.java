14dfc8fdc4a84c28afdecd0fe2cdac04df5e9e4f
Lubov Avduhova <loveavduhova@gmail.com>
2025-08-14 10:46:55 +0700

    Two Sum II — Input Array Is Sorted - реализация

---
+++ src/test/java/org/example/intern/project/TwoSumUtilsTest.java
@@ -0,0 +1,35 @@
+package org.example.intern.project;
+
+import org.example.intern.project.algo.TwoSumUtils;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TwoSumUtilsTest {
+    //Пара в начале
+    @Test
+    public void inputArrayIsSorted_shouldReturnPair_whenPairIsAtTheBeginning() {
+        int[] nums = new int[]{1, 2, 4, 8, 10};
+        int target = 3;
+
+        Assertions.assertArrayEquals(new int[]{0, 1}, TwoSumUtils.inputArrayIsSorted(nums, target));
+    }
+
+
+    //Пара в середине
+    @Test
+    public void inputArrayIsSorted_shouldReturnPair_whenPairIsAtTheMiddle() {
+        int[] nums = new int[]{1, 3, 5, 7, 9};
+        int target = 12;
+
+        Assertions.assertArrayEquals(new int[]{1, 4}, TwoSumUtils.inputArrayIsSorted(nums, target));
+    }
+
+    //Пара в конце
+    @Test
+    public void inputArrayIsSorted_shouldReturnPair_whenPairIsAtTheEnd() {
+        int target = 18;
+        int[] nums = new int[]{2, 4, 6, 8, 10};
+
+        Assertions.assertArrayEquals(new int[]{3, 4}, TwoSumUtils.inputArrayIsSorted(nums, target));
+    }
+}
